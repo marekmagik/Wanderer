84e993ad3f5599cb9f72ec56a553d2310e0c6e00
marcin <marbogster@gmail.com>
2014-06-08 23:53:44 +0200

    Dodany (wstepny) widoczek do wyswietlania listy punktow i podstawowa logika do ich pobrania.

---
+++ Wanderer/ListOfPlaces.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using System.IO;
+
+namespace Wanderer
+{
+    public partial class ListOfPlaces : PhoneApplicationPage
+    {
+        private List<Place> places;
+
+        public ListOfPlaces()
+        {
+            places = new List<Place>();
+            GetData(20.5,40.6,10000);
+            this.DataContext = places;
+
+            InitializeComponent();
+
+        }
+
+        //tymczasowo, zeby cos sie wypisalo, na razie jeszcze nie ma sciagania obrazka
+        private void GetData(double lon, double lat, int distance)
+        {
+            Place place = new Place();
+            place.Desc = " Super Gory";
+            place.Distance = 1.0;
+
+            places.Add(place);
+
+            Place place2 = new Place();
+            place2.Desc = " Bar mleczny ";
+            place.Distance = 2.6;
+
+            places.Add(place2);
+        }
+
+        // nie przetestowane, testowalem na desktopie i dzialalo ale tam sie to robilo inaczej wiec ten kod moze byc wyjatkogenny ;)
+        // zamiast localhost oczywiscie jakis tam adres :P
+        /*private void GetData(double lon, double lat, int distance)
+        {
+            string uri = "http://localhost:7001/Wanderer/api/places/get/" + lon + "/" + lat + "/" + distance;
+            HttpWebRequest request =
+                (HttpWebRequest)HttpWebRequest.Create(uri);
+            request.BeginGetResponse(RequestCallback, request);
+        }
+
+
+        void RequestCallback(IAsyncResult result)
+        {
+            HttpWebRequest request = result.AsyncState as HttpWebRequest;
+            if (request != null)
+            {
+                try
+                {
+                    WebResponse response = request.EndGetResponse(result);
+                    Stream stream = response.GetResponseStream();
+                    StreamReader streamReader = new StreamReader(stream);
+                    string json = streamReader.ReadToEnd();
+                    JSONParser parser = new JSONParser();
+                    places = parser.ParsePlacesJSON(json);
+                }
+                catch (WebException e)
+                {
+                    return;
+                }
+            }
+        }*/
+    }
+}
---
+++ Wanderer/Place.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wanderer
+{
+    class Place
+    {
+        public int PlaceId { get; set; }
+        public double Lon { get; set; }
+        public double Lat { get; set; }
+        public string Desc { get; set; }
+        public double Distance { get; set; }
+
+        public void Print()
+        {
+            Console.WriteLine(PlaceId + " " + Lon + " " + Lat + " " + Desc+ " " + Distance);
+        }
+
+    }
+}
