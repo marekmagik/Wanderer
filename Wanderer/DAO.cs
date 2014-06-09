e2da97cf31bec3cbf038ee7b76d38aa169a63345
marcin <marbogster@gmail.com>
2014-06-09 19:46:04 +0200

    Przeniesienie czesci llogiki do dao

--- Wanderer/DAO.cs
+++ Wanderer/DAO.cs
@@ -1,14 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
 
 namespace Wanderer
 {
     
     public class DAO
     {
+
+        private List<Place> places;
+        private const string address = "10.20.120.151";//"10.22.115.27";.
+        private int actualIndex;
 
         public static List<ImageMetadata> getPointsInRange(double longitude, double latitude, double range)
         {
@@ -26,6 +33,92 @@ namespace Wanderer
             listOfMetadata.Add(image1);
 
             return listOfMetadata;
+        }
+
+        public void GetDataFromServer(List<Place> places, double lon, double lat, int distance)
+        {
+            this.places = places;
+            string longitude = Convert.ToString(lon).Replace(',', '.');
+            string latitude = Convert.ToString(lat).Replace(',', '.');
+
+            string uri = "http://" + address + ":7001/Wanderer/api/places/get/" + longitude + "/" + latitude + "/" + distance;
+
+            HttpWebRequest request =
+                (HttpWebRequest)HttpWebRequest.Create(uri);
+            request.Method = "GET";
+            request.BeginGetResponse(new AsyncCallback(RequestCallback), request);
+
+        }
+
+
+        void RequestCallback(IAsyncResult result)
+        {
+            HttpWebRequest request = result.AsyncState as HttpWebRequest;
+            if (request != null)
+            {
+                try
+                {
+                    WebResponse response = request.EndGetResponse(result);
+                    Stream stream = response.GetResponseStream();
+                    StreamReader streamReader = new StreamReader(stream);
+                    string json = streamReader.ReadToEnd();
+                    JSONParser parser = new JSONParser();
+                    places = parser.ParsePlacesJSON(json);
+
+                    actualIndex = 0;
+                    LoadAllImages();              
+  
+                }
+                catch (WebException e)
+                {
+                    return;
+                }
+            }
+        }
+
+        private void LoadAllImages()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void LoadImage()
+        {
+            string uri = "http://" + address + ":7001/Wanderer/api/photos/get/thumbnail/" + places.ElementAt(actualIndex).PlaceId;
+            HttpWebRequest request =
+                (HttpWebRequest)HttpWebRequest.Create(uri);
+            request.BeginGetResponse(ThumbRequestCallback, request);
+        }
+
+        void ThumbRequestCallback(IAsyncResult result)
+        {
+            HttpWebRequest request = result.AsyncState as HttpWebRequest;
+            if (request != null)
+            {
+                try
+                {
+                    WebResponse response = request.EndGetResponse(result);
+                    Stream stream = response.GetResponseStream();
+                    BitmapImage image = new BitmapImage();
+                    image.SetSource(stream);
+                    places.ElementAt(actualIndex).Image = image;
+
+                    if (actualIndex == places.Count)
+                    {
+                        //callback
+                    }
+                    else
+                    {
+                        actualIndex++;
+                        LoadImage();
+                    }
+                      
+
+                }
+                catch (WebException e)
+                {
+                    return;
+                }
+            }
         }
 
     }
--- Wanderer/ListOfPlaces.xaml.cs
+++ Wanderer/ListOfPlaces.xaml.cs
@@ -18,140 +18,24 @@ namespace Wanderer
     {
         private List<Place> places;
         private List<ImageMetadata> points;
-        private const string address = "10.20.120.151";//"10.22.115.27";.
         private Place actualPlace;
 
         public ListOfPlaces()
         {
             InitializeComponent();
             places = new List<Place>();
-            GetDataFromServer(20.5,40.6,1000000);
+            DAO dao = new DAO();
+            dao.GetDataFromServer(places,20.5,40.6,1000000);
             this.DataContext = places;
-            this.points = DAO.getPointsInRange(20.5, 40.6, 1000000);
+            //this.points = dao.getPointsInRange(20.5, 40.6, 1000000);
             //this.DataContext = points;
         }
 
-        //tymczasowo, zeby cos sie wypisalo, na razie jeszcze nie ma sciagania obrazka
-        private void GetData(double lon, double lat, int distance)
-        {
-            Place place = new Place();
-            place.Desc = " Super Gory";
-            place.Distance = 1.0;
-            place.Image = null;
-
-            places.Add(place);
-
-            Place place2 = new Place();
-            place2.Desc = " Bar mleczny ";
-            place.Distance = 2.6;
-            place.Image = null;
-
-            places.Add(place2);
-        }
-
-        // nie przetestowane, testowalem na desktopie i dzialalo ale tam sie to robilo inaczej wiec ten kod moze byc wyjatkogenny ;)
-        // zamiast localhost oczywiscie jakis tam adres :P
-        private void GetDataFromServer(double lon, double lat, int distance)
-        {
-            //string uri = "http://"+address+":7001/Wanderer/api/places/get/" + lon + "/" + lat + "/" + distance;
-            string longitude = Convert.ToString(lon).Replace(',', '.');
-            string latitude = Convert.ToString(lat).Replace(',', '.');
-
-            string uri = "http://" + address + ":7001/Wanderer/api/places/get/" + longitude + "/" + latitude + "/" + distance;
-
-            HttpWebRequest request =
-                (HttpWebRequest)HttpWebRequest.Create(uri);
-            request.Method = "GET";
-            request.BeginGetResponse(new AsyncCallback(RequestCallback), request);
-
-        }
-
-
-        void RequestCallback(IAsyncResult result)
-        {
-            Debug.WriteLine("Hello2");
-            HttpWebRequest request = result.AsyncState as HttpWebRequest;
-            if (request != null)
-            {
-                try
-                {
-                    Debug.WriteLine("Hello!");
-                    WebResponse response = request.EndGetResponse(result);
-                    Stream stream = response.GetResponseStream();
-                    StreamReader streamReader = new StreamReader(stream);
-                    string json = streamReader.ReadToEnd();
-                    JSONParser parser = new JSONParser();
-                    List<Place> places2 = parser.ParsePlacesJSON(json);
-                    foreach (Place p in places2)
-                    {
-                        places.Add(p);
-                        Debug.WriteLine(p);
-                    }
-                   
-                    LoadImage(places.ElementAt(4));
-
-                    Deployment.Current.Dispatcher.BeginInvoke(delegate
+        /*Deployment.Current.Dispatcher.BeginInvoke(delegate
                     {
                         PlacesListBox.ItemsSource = null;
                         PlacesListBox.ItemsSource = places2;
-                    });
-                    
-                   
-                    // BeginLoadingImages();
-                     //   
-                }
-                catch (WebException e)
-                {
-                    return;
-                }
-            }
-        }
-
-
-
-        private void BeginLoadingImages(){
-        }
-
-        private void LoadImage(Place place)
-        {
-            actualPlace = place;
-            string uri = "http://" + address + ":7001/Wanderer/api/photos/get/thumbnail/" + place.PlaceId;
-            HttpWebRequest request =
-                (HttpWebRequest)HttpWebRequest.Create(uri);
-            request.BeginGetResponse(ThumbRequestCallback, request);
-        }
-
-        void ThumbRequestCallback(IAsyncResult result)
-        {
-            HttpWebRequest request = result.AsyncState as HttpWebRequest;
-            if (request != null)
-            {
-                try
-                {
-                    WebResponse response = request.EndGetResponse(result);
-                    Stream stream = response.GetResponseStream();
-                   // BitmapImage image = new BitmapImage();
-                   // image.SetSource(stream);
-                   // actualPlace.Image = image;
-
-                    Deployment.Current.Dispatcher.BeginInvoke(delegate
-                    {
-                        BitmapImage image = new BitmapImage();
-                        image.SetSource(stream);
-                        actualPlace.Image = image;
-
-                        PlacesListBox.ItemsSource = null;
-                        PlacesListBox.ItemsSource = places;
-                    });
-
-                    Debug.WriteLine("obrazek pobrany!");
-
-                }
-                catch (WebException e)
-                {
-                    return;
-                }
-            }
-        }
+                    });*/
+        
     }
 }
