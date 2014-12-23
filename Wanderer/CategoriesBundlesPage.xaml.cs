0746145621a31513da199496127662e55ad573bf
marcin <marbogster@gmail.com>
2014-12-23 02:27:05 +0100

    Bug

--- Wanderer/CategoriesBundlesPage.xaml.cs
+++ Wanderer/CategoriesBundlesPage.xaml.cs
@@ -318,7 +318,7 @@ namespace Wanderer
         {
             Grid grid = (Grid)sender;
             TextBlock textBlock = (TextBlock)grid.Children[0];
-            if(!_gridMap.ContainsKey(textBlock.Text)
+            if(!_gridMap.ContainsKey(textBlock.Text))
                 _gridMap.Add(textBlock.Text, grid);
             if (grid.Children[1] is Button)
             {
